2b13bbe0273240221749e3a017d2d546c8e3f355
Bruno Juncklaus Martins <bruno.martins@nexxera.com>
2017-08-15 10:01:33 -0300

    Adding comments and one more example of resize method.

--- resize_thumbnail.py
+++ resize_thumbnail.py
@@ -10,7 +10,8 @@ for f in files:
 	print(f)
 	fd_img = open(dir + f, 'r+b')
 	img = Image.open(fd_img)
-	img = resizeimage.resize_thumbnail(img, [100, 100])
-	# img = resizeimage.resize_crop(img, [32, 32])
+	img = resizeimage.resize_thumbnail(img, [100, 100]) # this keeps the ratio
+	# img = resizeimage.resize_crop(img, [32, 32]) # this crops the image creating a box with the given dimensions
+	# img = resizeimage.resize_contain(img, [32, 32]) # resize the image so that it can fit in the specified area, keeping the ratio and without crop
 	img.save(dir_output + f, img.format)
 	fd_img.close()
