4254a0bcbd64b33920777eb6eea0ab09f8c14452
Bruno Juncklaus <bjuncklaus@gmail.com>
2017-08-18 08:53:27 -0300

    Adding pooling layer for the first convolutional layer.

--- main.py
+++ main.py
@@ -18,8 +18,11 @@ def deepnn(x):
   # Last dimension is for "features" - there is only one here, since images are
   # grayscale -- it would be 3 for an RGB image, 4 for RGBA, etc.
   x_image = tf.reshape(x, [-1, 28, 28, 1])
-  
+
   # First convolutional layer - maps one grayscale image to 32 feature maps.
   W_conv1 = weight_variable([5, 5, 1, 32])
   b_conv1 = bias_variable([32])
   h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+
+  # Pooling layer - downsamples by 2X.
+  h_pool1 = max_pool_2x2(h_conv1)
