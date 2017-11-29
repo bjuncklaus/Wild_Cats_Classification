8deae02dc8d26cb1740b49cedad7fa6335a5dd52
Bruno Juncklaus <bjuncklaus@gmail.com>
2017-11-29 08:47:22 -0200

    Gitignore, grayscale algorithm and a few examples.

--- image_grayscale.py
+++ image_grayscale.py
@@ -1,1 +1,15 @@
-https://stackoverflow.com/questions/12201577/how-can-i-convert-an-rgb-image-into-grayscale-in-python
+from PIL import Image
+import os
+
+source_dir = 'images/oncas/'
+destination_dir = 'images/greyscaled/'
+
+if not os.path.exists(destination_dir):
+    os.mkdir(destination_dir)
+
+files = [f for f in os.listdir(source_dir)]
+
+for f in files:
+    print('Found image:', f)
+    img = Image.open(source_dir + f).convert('L')
+    img.save(destination_dir + f)
